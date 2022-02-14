4efdfd2e43e4b44d90dcc3ac3fd6212e53edc768
User Macbook <usermacbook@mbp-de-user.home>
2022-02-14 00:08:42 +0000

    alrady  frist commit

--- src/pass.php
+++ src/pass.php
@@ -13,6 +13,12 @@ include "uploader.php";
 
     <link rel="canonical" href="https://getbootstrap.com/docs/5.1/examples/sticky-footer-navbar/">
     <link href="style.css" rel="stylesheet">
+    <link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png">
+    <link rel="manifest" href="favicon/site.webmanifest">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="theme-color" content="#ffffff">
   </head>
 <body class="d-flex flex-column h-100">
     
--- src/public_khint_esp.php
+++ src/public_khint_esp.php
@@ -12,6 +12,12 @@ $methode= $_SERVER["REQUEST_METHOD"];
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>    <link href="public.css" rel="stylesheet">
     <title>HINTSxHINTS</title>
+    <link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png">
+    <link rel="manifest" href="favicon/site.webmanifest">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="theme-color" content="#ffffff">
 </head>
 <body>
     <header >
@@ -43,7 +49,7 @@ $methode= $_SERVER["REQUEST_METHOD"];
         </nav>
     </header>
     <?php
-       $select = "select * from EspKhints ";
+       $select = "select * from EspKhints order by cours";
        $result_select = $conn->query($select);
         echo "<div class=tableau y-10>";
         echo "<br>";
