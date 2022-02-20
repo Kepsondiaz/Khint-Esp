70a8705956b0927c887e5c9c94dbd35e6bc565b3
User Macbook <usermacbook@mbp-de-user.home>
2022-02-20 12:11:52 +0000

    fix conflicts

--- src/uploader.php
+++ src/uploader.php
@@ -1,26 +1,3 @@
-<<<<<<< HEAD
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-    <title>HINTSxHINTS</title>
-    <link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png">
-    <link rel="manifest" href="favicon/site.webmanifest">
-    <meta name="msapplication-TileColor" content="#da532c">
-    <meta name="theme-color" content="#ffffff">
-</head>
-<body>
-    
-</body>
-</html>
-=======
->>>>>>> master
 <?php
     include "connect_db.php";
     $file_name = $_FILES["fichier"]["name"];
@@ -28,7 +5,7 @@
     $file_tmp_name = $_FILES["fichier"]["tmp_name"];
     $niveau=$_POST["niveau"];
     $cours=$_POST["cours"];
-    $file_public ='../Public/'.$file_name;
+    $file_public ='../public/'.$file_name;
     $extension_autorisees = array(".pdf", ".PDF", ".zip");
 
     if (in_array($file_extension, $extension_autorisees))
